c72b790bc0b032dfbb4e708f7a786598fb5756a8
kitanablade <kiteagle@gmail.com>
2022-08-01 19:51:58 -0700

    models index updated

--- models/index.js
+++ models/index.js
@@ -1,14 +1,14 @@
-//NOTE: replace the index.js file in the Develop/models folder with this one.  
-
-// import models
+ // import models
 const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category);
 
 // Categories have many Products
+Category.hasMany(Product);
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
